dba4f6e1f7acbf1718274e48c01ed96d816883aa
zeyad-boyah <zeyadelgohary.ze@gmail.com>
2025-03-07 02:24:07 +0200

    test more about requests

--- backend/simple_api/views.py
+++ backend/simple_api/views.py
@@ -1,4 +1,15 @@
 from django.http import JsonResponse
+import json
 
-def api_home(request):
-    return JsonResponse({'message':"hallo, welkommen von Django API"})
+def api_home(request, *args, **kwargs):
+    body = request.body # string of json data
+    try:
+        data = json.loads(body) # string of json -> python dict
+    except:
+        pass
+    print(data)
+    data["params"]= dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -9,8 +9,8 @@ and using localhost
 endpoint = "http://localhost:8000/simple_api/"
 
 
-get_response = requests.get(endpoint)
+get_response = requests.get(endpoint, params={"abc":"123"}, json={"query":"hello from query"})
 
-print(get_response.json()['message'])
-# print(get_response.text)
+# print(get_response.json()['message'])
+print(get_response.text)
 # print(get_response.status_code)
