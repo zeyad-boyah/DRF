459f4ce05ae3ac4db3ed0836903ac8be366f94b6
zeyad-boyah <zeyadelgohary.ze@gmail.com>
2025-03-07 02:02:39 +0200

    first draft from api_home

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,12 +3,14 @@ import requests
 
 """
 this is a simple client in python intended to test http response 
-and using httpbin.org
+and using localhost
 """
-endpoint = "https://httpbin.org/anything"
+# endpoint = "http://localhost:8000"
+endpoint = "http://localhost:8000/simple_api/"
+
 
 get_response = requests.get(endpoint)
 
-print(get_response.json())
+print(get_response.json()['message'])
 # print(get_response.text)
 # print(get_response.status_code)
