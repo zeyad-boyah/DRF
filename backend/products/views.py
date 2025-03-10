21df711902c00a7524d8a63a4eb3edbfa28cd948
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-11 00:37:38 +0200

    add alt view's GET method

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework import generics
 from products.models import Product
 from products.serializers import ProductSerializer
+
+from django.shortcuts import get_object_or_404 
+
 
 # Create your views here.
 class ProductDetailAPIView(generics.RetrieveAPIView):
@@ -23,3 +28,13 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         if description is None:
             description = title
         serializer.save(description=description)
+
+@api_view(["GET", "POST"])
+def alt_view_or_create_API(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == "GET":
+        if not pk == None:
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
