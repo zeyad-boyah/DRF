d35a6a9e3665ef2286b1863262ee0b10fe27ee7a
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-15 00:39:07 +0200

    add ProductListCreateDetailMixin

--- py_client/create.py
+++ py_client/create.py
@@ -5,7 +5,7 @@ endpoint = "http://localhost:8000/simple_api/products/"
 
 data = {
     "title" : "the lost imposter",
-    "description" : "hope he comes home",
+    
     "price" : "69"
 }
 get_response = requests.post(endpoint, json= data )
